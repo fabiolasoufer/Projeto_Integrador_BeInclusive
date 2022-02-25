ed900f6ebd0b87d1b5f2b3b8cf96bfee14251e57
sotie <javacursobustos@gmail.com>
2022-02-25 10:40:38 -0300

    @bustos00 [Task:Finalizando Controller]

--- beinclusive/src/main/java/org/beinclusive/beinclusive/controller/TemaController.java
+++ beinclusive/src/main/java/org/beinclusive/beinclusive/controller/TemaController.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.beinclusive.beinclusive.model.Tema;
 import org.beinclusive.beinclusive.repository.TemaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +27,28 @@ public class TemaController {
 	
 	@GetMapping
    public ResponseEntity<List<Tema>> getAll(){
-	   return ResponseEntity.ok(repository.findAll());
-	   
+	   return ResponseEntity.ok(repository.findAll());	   
    }
+	@GetMapping ("/{id}")
+	public ResponseEntity<Tema> GetById(@PathVariable(value = "id") long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());	 
+	}
+	@GetMapping("/tema/{avaliacao}")
+	public ResponseEntity<List<Tema>> getByAvaliacao(@PathVariable String avaliacao){
+		return ResponseEntity.ok(repository.findAllByAvaliacao(avaliacao));
+	}
+	@PostMapping
+	public ResponseEntity<Tema> post (@RequestBody Tema tema){
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(repository.save(tema)); 
+	}
+	@PutMapping
+	public ResponseEntity<Tema> put (@RequestBody Tema tema){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
+	}
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) 
+	{
+		repository.deleteById(id);
+	}
 }
