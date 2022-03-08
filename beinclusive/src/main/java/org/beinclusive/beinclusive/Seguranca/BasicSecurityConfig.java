b116e3c44cdde58d51ff6bbceda164a359f25a39
unknown <samara_alves28@hotmail.com>
2022-03-08 10:34:47 -0300

    @Samara-Alves [Task: Criando camada UserDetailsImpl]

--- beinclusive/src/main/java/org/beinclusive/beinclusive/Seguranca/BasicSecurityConfig.java
+++ beinclusive/src/main/java/org/beinclusive/beinclusive/Seguranca/BasicSecurityConfig.java
@@ -30,7 +30,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.POST, "/usuarios/logar").permitAll()
+		.antMatchers(HttpMethod.PUT, "/usuarios/logar").permitAll()
 		.antMatchers(HttpMethod.POST, "/usuario/cadastrar").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
